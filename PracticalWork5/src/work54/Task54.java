62ab992c519faf4adfba4c103f1dae6cd7abb374
U-Vit <85488942+U-Vit@users.noreply.github.com>
2021-06-21 22:48:36 +0300

    Add files via upload

--- PracticalWork5/src/work54/Task54.java
+++ PracticalWork5/src/work54/Task54.java
@@ -2,6 +2,35 @@ package work54;
 
 public class Task54 {
     public static void main(String[] args) {
+        String str1 = "Cartoon", str2 = "Tomcat";
+        int i = 0;
+
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        System.out.println(str1);
+        System.out.println(str2);
+
+        while(i < str1.length()){
+
+            int j = 0;
+            char chr = str1.charAt(i);
+
+            while(j < str2.length()){
+
+                if(chr == str2.charAt(j)){
+                    break;
+                }
+                else if (j == str2.length() - 1) {
+
+                    System.out.println(chr);
+                }
+                j++;
+            }
+
+            i++;
+        }
+
 
     }
 }
--- PracticalWork5/src/work55/Task55.java
+++ PracticalWork5/src/work55/Task55.java
@@ -3,5 +3,18 @@ package work55;
 public class Task55 {
     public static void main(String[] args) {
 
+        String str = "football";
+        int i = 0;
+
+        while (i < str.length()){
+
+            char chr  = str.charAt(i);
+
+            if(str.indexOf(chr) == str.lastIndexOf(chr)){
+                System.out.println(chr);
+            }
+
+            i++;
+        }
     }
 }
